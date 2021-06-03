c54d70d1c02c1125c35143fafbee30797be592e7
Sanjana <sanjana99.r@gmail.com>
2021-06-03 21:29:55 +0530

    4th

--- displa.py
+++ displa.py
@@ -1,17 +1,17 @@
 from flask import Flask, redirect, url_for, render_template,request
 
 app = Flask(__name__)
-@app.route("/")  # this sets the route to this page
+@app.route("/",methods=['GET','POST'])  # this sets the route to this page
 def home():
     if request.method=='POST':
         k=request.form['link']
         return redirect(url_for('index',ty=k))
     else:
         return render_template("index.html") # some basic inline html
-
+@app.route("/index/<ty>",methods=['GET','POST'])  # this sets the route to this page
 def index(ty):
     if request.method=='GET':
-        print("got")
+        print("got",ty)
     else:
         print("list(books['name.1'])")
 if __name__ == "__main__":
