8f78e6d68bac75693c23dee2317f9c5ab102fead
Sanjana <sanjana99.r@gmail.com>
2021-06-04 00:30:19 +0530

    -new

--- displa.py
+++ displa.py
@@ -1,18 +1,21 @@
 from flask import Flask, redirect, url_for, render_template,request
+from summarizer import summarize as s
 
 app = Flask(__name__)
 @app.route("/",methods=['GET','POST'])  # this sets the route to this page
 def home():
     if request.method=='POST':
-        k=request.form['link']
-        return redirect(url_for('index',ty=k))
+        link=request.form['link']
+        return redirect(url_for('index',link=link))
     else:
         return render_template("index.html") # some basic inline html
-@app.route("/index/<ty>",methods=['GET','POST'])  # this sets the route to this page
-def index(ty):
+@app.route("/index/<link>",methods=['GET','POST'])  # this sets the route to this page
+def index(link):
     if request.method=='GET':
-        print("got",ty)
+        print("Got link ",link)
+        s.summarizer(link,2)
+        return render_template("video.html")
     else:
-        print("list(books['name.1'])")
+        return render_template("video.html") # some basic inline html
 if __name__ == "__main__":
     app.run(debug=True)
